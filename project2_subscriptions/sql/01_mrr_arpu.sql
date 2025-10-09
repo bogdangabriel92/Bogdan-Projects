7f754d6dbda728f3ae3fdbdaf4c6d923f9290ebc
bogdangabriel92 <bogdan.gabriel.rasu@gmail.com>
2025-10-09 21:15:21 +0300

    Delete project2_subscriptions directory

--- project2_subscriptions/sql/01_mrr_arpu.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  DATE_TRUNC('month', period_start) AS month,
-  SUM(mrr) AS mrr,
-  SUM(mrr) / NULLIF(COUNT(DISTINCT customer_id), 0) AS arpu
-FROM subscriptions
-WHERE is_active = TRUE
-GROUP BY DATE_TRUNC('month', period_start)
-ORDER BY month;
