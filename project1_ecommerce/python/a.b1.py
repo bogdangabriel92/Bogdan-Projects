b10cc006d5e477e453228e38b579740168bf6e54
bogdangabriel92 <bogdan.gabriel.rasu@gmail.com>
2025-11-05 00:27:13 +0200

    Delete project1_ecommerce directory

--- project1_ecommerce/python/a.b1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from scipy.stats import chi2_contingency
-
-# Agregare sesiuni
-ab = pd.read_csv("sessions_ab.csv")  # is_variant_B, converted
-tab = pd.crosstab(ab["is_variant_B"], ab["converted"])
-chi2, p, dof, exp = chi2_contingency(tab)
-print("Chi-square p-value:", p)
--- project1_ecommerce/sql/01_aov_daily.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  CAST(order_date AS DATE) AS day,
-  COUNT(DISTINCT order_id) AS orders,
-  SUM(revenue) AS revenue,
-  AVG(revenue) AS aov
-FROM orders
-GROUP BY CAST(order_date AS DATE)
-ORDER BY day;
